b28d7028a1aa66bb23de0f13e89a90b30276be02
Muhammad Wasif <mianwasif.001@gmail.com>
2024-11-28 00:40:47 +0500

    icon animation

--- src/components/TrustedClient.tsx
+++ src/components/TrustedClient.tsx
@@ -8,48 +8,29 @@ import logoStatamic from '@/images/logos/statamic.svg'
 import logoStaticKit from '@/images/logos/statickit.svg'
 import logoTransistor from '@/images/logos/transistor.svg'
 import logoTuple from '@/images/logos/tuple.svg'
-import TextAnimation from './TextAnimation'
 
 export function TrustedClient() {
+  const logos = [
+    { name: 'Transistor', logo: logoTransistor },
+    { name: 'Tuple', logo: logoTuple },
+    { name: 'StaticKit', logo: logoStaticKit },
+    { name: 'Mirage', logo: logoMirage },
+    { name: 'Laravel', logo: logoLaravel },
+    { name: 'Statamic', logo: logoStatamic },
+    { name: 'StaticKit', logo: logoStaticKit },
+    { name: 'Mirage', logo: logoMirage },
+  ];
+
   return (
-    <Container className="pb-32 text-center ">
+    <Container className="pb-32 text-center">
       <div className="mt-36 overflow-hidden lg:mt-10">
         <h2 className="font-display text-2xl text-[#ffffffa2]">
           Trusted by teams at
         </h2>
-        <div className="mt-8 flex space-x-8 sm:flex-col sm:space-x-0 sm:space-y-10 xl:flex-row xl:space-y-0">
-          {/* <div className="animate-marquee flex w-max">
-            {[
-              { name: 'Transistor', logo: logoTransistor },
-              { name: 'Tuple', logo: logoTuple },
-              { name: 'StaticKit', logo: logoStaticKit },
-              { name: 'Mirage', logo: logoMirage },
-              { name: 'Laravel', logo: logoLaravel },
-              { name: 'Statamic', logo: logoStatamic },
-            ].map((company, index) => (
-              <div
-                key={index}
-                className="flex flex-shrink-0 items-center justify-center gap-x-8 sm:gap-y-10"
-              >
-                <Image src={company.logo} alt={company.name} unoptimized />
-              </div>
-            ))}
-          </div> */}
+        <div className="mt-14 flex space-x-8 sm:flex-col sm:space-x-0 sm:space-y-10 xl:flex-row xl:space-y-0">
           <div className="animate-marquee flex w-max gap-10">
-            {[
-              { name: 'Transistor', logo: logoTransistor },
-              { name: 'Tuple', logo: logoTuple },
-              { name: 'StaticKit', logo: logoStaticKit },
-              { name: 'Mirage', logo: logoMirage },
-              { name: 'Laravel', logo: logoLaravel },
-              { name: 'Statamic', logo: logoStatamic },
-              { name: 'StaticKit', logo: logoStaticKit },
-              { name: 'Mirage', logo: logoMirage },
-            ].map((company, index) => (
-              <div
-                key={index}
-                className="flex flex-shrink-0 items-center justify-center gap-x-8 sm:gap-y-10"
-              >
+            {logos.concat(logos).map((company, index) => ( 
+              <div key={index} className="flex flex-shrink-0 items-center justify-center gap-x-8 sm:gap-y-10">
                 <Image src={company.logo} alt={company.name} unoptimized />
               </div>
             ))}
@@ -57,5 +38,5 @@ export function TrustedClient() {
         </div>
       </div>
     </Container>
-  )
+  );
 }
